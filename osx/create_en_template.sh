cc06c998df71a640be7c27d7727625f3290d759b
Jason Sherman <jsnshrmn@users.noreply.github.com>
2014-12-10 10:06:20 -0600

    Update create_en_template.sh
    Changed to specified user account instead of current

--- osx/create_en_template.sh
+++ osx/create_en_template.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
 
-## Create english language template based on current user account
+## Create english language template based on specified account
 ## Directly inspired by Jason Watkins howto
 ## https://sites.sas.upenn.edu/jasonrw/blog/2013/03/create-custom-default-user-profile-os-x-107108
+die () {
+    echo >&2 "$@"
+    exit 1
+}
 
-TEMPLATE_SOURCE=$USER
+## need to be root
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
 
-## Need to be root
-sudo su
+## user account required
+[ "$#" -eq 1 ] || die "1 argument required, $# provided"
+
+TEMPLATE_SOURCE=$1
+
 
 ## Backup old default profile
 cp -r /System/Library/User\ Template/English.lproj /System/Library/User\ Template/English.bak
