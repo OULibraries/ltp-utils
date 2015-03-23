afd0a440b47bf2f0ab404d0be3798885a4fcf7e5
Jason Sherman <jsnshrmn@users.noreply.github.com>
2015-03-23 15:32:10 -0500

    fixed some order of operations issues.

--- bootstrap_drupal.sh
+++ bootstrap_drupal.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
-## Build from drush make
+## Deploy from drush make
 
-SITESOWNER=apache:apache                        # unix owner and group of /srv.
-## Don't edit below here.
-## Requires argument
+# Owner and group for site path
+SITESOWNER=apache:apache
+
+# Set path and sudo string
+PATH=/usr/local/bin:/usr/bin:/bin:/sbin
+SUDO=''
+if (( $EUID != 0 )); then
+    SUDO='sudo'
+fi
+
+## Require argument
 if [ ! -z "$1" ]
 then
   SITEPATH=$1
@@ -13,23 +21,19 @@ else
   exit 1;
 fi
 
-# root DB password
+# Get root DB password
 echo -n Root DB Password:
 read -s ROOTDBPSSWD
 echo
 
 # Generate Drupal DB password
 DBPSSWD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
-PATH=/usr/local/bin:/usr/bin:/bin:/sbin
-SUDO=''
-if (( $EUID != 0 )); then
-    SUDO='/usr/bin/sudo'
-fi
 
 if [[ -e $SITEPATH ]]; then
     echo "$SITEPATH already exists!"
     exit 1
 fi
+
 ## Make the parent directory
 $SUDO mkdir -p $SITEPATH
 
@@ -45,14 +49,7 @@ $SUDO mv $SITEPATH/drupal/sites/default $SITEPATH/
 ## Link default site folder
 $SUDO ln -s $SITEPATH/default $SITEPATH/drupal/sites/default
 
-# Set SELinux and owner
-$SUDO chcon -R -t  httpd_sys_content_t $SITEPATH
-$SUDO chown -R $SITESOWNER $SITEPATH
-
-## Set perms - allows group write
-$SUDO find $SITEPATH -type d -exec chmod u=rwx,g=rwx,o= '{}' \;
-$SUDO find $SITEPATH -type f -exec chmod u=rw,g=rw,o= '{}' \;
-
+## Setup settings.php
 read -d '' SETTINGSPHP <<- EOF
 \$databases = array (
   'default' =>
@@ -70,13 +67,27 @@ read -d '' SETTINGSPHP <<- EOF
   ),
 );
 EOF
+
+## Write out settings.php
 $SUDO cp $SITEPATH/default/default.settings.php $SITEPATH/default/settings.php
 echo "$SETTINGSPHP" >> $SITEPATH/default/settings.php
+
 ## Create the Drupal database
 $SUDO drush sql-create --db-su=root --db-su-pw=$ROOTDBPSSWD -r $SITEPATH/drupal || exit 1;
+
 ## Do the Drupal install
 $SUDO drush -y -r $SITEPATH/drupal site-install --site-name=$SITE || exit 1;
+
 ## Make the apache config
 sudo sed "s/__SITE_DIR__/$SITE/g" /etc/httpd/conf.d/template_init_oulib_drupal > /etc/httpd/conf.d/srv_$SITE.conf
+
+# Set SELinux and owner
+$SUDO chcon -R -t  httpd_sys_content_t $SITEPATH
+$SUDO chown -R $SITESOWNER $SITEPATH
+
+## Set perms - allows group write
+$SUDO find $SITEPATH -type d -exec chmod u=rwx,g=rwx,o= '{}' \;
+$SUDO find $SITEPATH -type f -exec chmod u=rw,g=rw,o= '{}' \;
+
 $SUDO service httpd configtest || exit 1;
 $SUDO service httpd reload || exit 1;
