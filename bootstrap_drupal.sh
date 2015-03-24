79226b702655a43d3ba387cc89aafba4b33008d1
Jason Sherman <jsnshrmn@users.noreply.github.com>
2015-03-24 09:23:16 -0500

    Updated SELinux to be more durable.

--- bootstrap_drupal.sh
+++ bootstrap_drupal.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
-## Deploy from drush make
+## Build from drush make
 
-# Owner and group for site path
-SITESOWNER=apache:apache
-
-# Set path and sudo string
-PATH=/usr/local/bin:/usr/bin:/bin:/sbin
-SUDO=''
-if (( $EUID != 0 )); then
-    SUDO='sudo'
-fi
-
-## Require argument
+SITESOWNER=apache:apache                        # unix owner and group of /srv.
+## Don't edit below here.
+## Requires argument
 if [ ! -z "$1" ]
 then
   SITEPATH=$1
@@ -21,35 +13,54 @@ else
   exit 1;
 fi
 
-# Get root DB password
+# root DB password
 echo -n Root DB Password:
 read -s ROOTDBPSSWD
 echo
 
 # Generate Drupal DB password
 DBPSSWD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
+PATH=/usr/local/bin:/usr/bin:/bin:/sbin
+SUDO=''
+if (( $EUID != 0 )); then
+    SUDO='sudo'
+fi
 
 if [[ -e $SITEPATH ]]; then
     echo "$SITEPATH already exists!"
     exit 1
 fi
-
 ## Make the parent directory
 $SUDO mkdir -p $SITEPATH
+$SUDO chown $SITESOWNER $SITEPATH
+$SUDO chmod 775 $SITEPATH
 
 ## Grab the basename of the site to use in a few places.
 SITE=`basename $SITEPATH`
 
 ## Build from drush make
-$SUDO drush dl drupal --drupal-project-rename=drupal --destination=$SITEPATH || exit 1;
+drush -y dl drupal --drupal-project-rename=drupal --destination=$SITEPATH || exit 1;
+
+## Set perms- allows group write
+echo "Setting permissions."
+find $SITEPATH/drupal -type d -exec chmod u=rwx,g=rwx,o= '{}' \;
+find $SITEPATH/drupal -type f -exec chmod u=rw,g=rw,o= '{}' \;
+
+# Set SELinux or die
+echo "Setting SELinux policy."
+$SUDO semanage fcontext -a -t httpd_sys_content_t  "$SITEPATH/drupal(/.*)?" || exit 1;
+$SUDO restorecon -R $SITEPATH/drupal || exit 1;
 
 ##  Move the default site out of the build. This makes updates easier later.
-$SUDO mv $SITEPATH/drupal/sites/default $SITEPATH/
+echo "Moving default site out of build."
+mv $SITEPATH/drupal/sites/default $SITEPATH/
 
-## Link default site folder
-$SUDO ln -s $SITEPATH/default $SITEPATH/drupal/sites/default
+## Link default site folder. Doing this last ensures that our earlier recursive
+## operations aren't duplicating efforts.
+echo "Linking default site into build."
+ln -s $SITEPATH/default $SITEPATH/drupal/sites/default
+#$SUDO chown $SITESOWNER $SITEPATH/drupal/sites/default
 
-## Setup settings.php
 read -d '' SETTINGSPHP <<- EOF
 \$databases = array (
   'default' =>
@@ -68,26 +79,20 @@ read -d '' SETTINGSPHP <<- EOF
 );
 EOF
 
-## Write out settings.php
-$SUDO cp $SITEPATH/default/default.settings.php $SITEPATH/default/settings.php
+cp $SITEPATH/default/default.settings.php $SITEPATH/default/settings.php
 echo "$SETTINGSPHP" >> $SITEPATH/default/settings.php
 
-## Create the Drupal database
-$SUDO drush sql-create --db-su=root --db-su-pw=$ROOTDBPSSWD -r $SITEPATH/drupal || exit 1;
-
-## Do the Drupal install
-$SUDO drush -y -r $SITEPATH/drupal site-install --site-name=$SITE || exit 1;
-
-## Make the apache config
-sudo sed "s/__SITE_DIR__/$SITE/g" /etc/httpd/conf.d/template_init_oulib_drupal > /etc/httpd/conf.d/srv_$SITE.conf
-
-# Set SELinux and owner
-$SUDO chcon -R -t  httpd_sys_content_t $SITEPATH
+# Set owner
+echo "Changing owner."
 $SUDO chown -R $SITESOWNER $SITEPATH
 
-## Set perms - allows group write
-$SUDO find $SITEPATH -type d -exec chmod u=rwx,g=rwx,o= '{}' \;
-$SUDO find $SITEPATH -type f -exec chmod u=rw,g=rw,o= '{}' \;
+## Create the Drupal database
+drush -y sql-create --db-su=root --db-su-pw=$ROOTDBPSSWD -r $SITEPATH/drupal || exit 1;
 
+## Do the Drupal install
+drush -y -r $SITEPATH/drupal site-install --site-name=$SITE || exit 1;
+
+## Make the apache config
+$SUDO sed "s/__SITE_DIR__/$SITE/g" /etc/httpd/conf.d/template_init_oulib_drupal > /etc/httpd/conf.d/srv_$SITE.conf
 $SUDO service httpd configtest || exit 1;
 $SUDO service httpd reload || exit 1;
